26bd47bb4a97480a0b5618d6a373a0e19f43784b
Nav Dhamu <n4vdeep@gmail.com>
2020-11-15 22:34:41 +0000

    added rest api view for tweet_detail_view

--- tweets/views.py
+++ tweets/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, Http404, JsonResponse
 from django.shortcuts import render
 
 from .models import Tweet
@@ -8,9 +8,23 @@ def home_view(request, *args, **kwargs):
     return HttpResponse("<h1>Hello World</h1>")
 
 def tweet_detail_view(request, tweet_id, *args, **kwargs):
+    """
+    REST API VIEW
+    return json data
+    So we can consume by JavaScript or Swift/Java/iOS/Android
+    """
+    data = {
+        'id':tweet_id,
+    }
+
+    status = 200
     try:
         obj = Tweet.objects.get(id=tweet_id)
+        # put a key of content with a value of obj.content into data dict
+        data['content'] = obj.content
     except:
-        raise Http404
+        # put a key of message with a value of Not found into data dict
+        data['message'] = 'Not found'
+        status = 404
     
-    return HttpResponse(f"<h1>Hello {tweet_id} - {obj.content}</h1>")
+    return JsonResponse(data, status=status) # similar to json.dumps with content_type='application/json' from python module
